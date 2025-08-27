80bded503954dc59496304c1014141addc4029c5
Hazrat Umar <13234hazrat@gmail.com>
2025-08-27 11:18:35 +0500

    fix the bugs and complete the js code

--- project_02/script.js
+++ project_02/script.js
@@ -1,20 +1,58 @@
-const textInput = document.getElementById("todo-input");
-const addBtn = document.getElementById("addBtn");
-const taskList = document.getElementById("taskList");
+document.addEventListener("DOMContentLoaded", () => {
+  const taskText = document.getElementById("todo-input");
+  const taskAddBtn = document.getElementById("addBtn");
+  const taskList = document.getElementById("taskList");
 
-let listArray = []
+  let tasks = JSON.parse(localStorage.getItem("task")) || [];
+
+  tasks.forEach( (task) => (renderTask(task)))
+
+  taskAddBtn.addEventListener("click", () => {
+    const textValue = taskText.value.trim()
+    if(textValue === "") return alert("please enter a task");
+
+    const newTask = {
+      id: Date.now(),
+      text: textValue,
+      completed: false,
+    }
+
+    tasks.push(newTask);
+    // console.log(tasks)
+    saveTask()
+    renderTask(newTask)
 
 
+    taskText.value = ""
+  })
 
-addBtn.addEventListener("click", () => {
-    const inputValue = textInput.value.trim()
-    if(inputValue === "") return
+  function saveTask() {
+    localStorage.setItem("task", JSON.stringify(tasks));
+  }
 
-    listArray.push(inputValue)
-    localStorage.setItem("task", JSON.stringify(listArray))
+  function renderTask(task){
+    const list = document.createElement("li");
+    list.setAttribute("data-id", task.id);
+    // console.log(list)
+    if(list.completed) list.classList.add("completed");
+    list.classList.add("task")
+    list.innerHTML = `
+    ${task.text} <button class="deleteBtn">Delete</button>`;
+
+    list.addEventListener("click", (e) => {
+      if(e.target.tagName === "BUTTON") return
+      task.completed = !task.completed;
+      list.classList.toggle("completed");
+    });
+
+    list.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter( (t) => t.id !== task.id);
+      list.remove()
+      saveTask()
+    })
+
+    taskList.appendChild(list)
+  }
 })
 
-
-function renderTask(){
-    
-}
