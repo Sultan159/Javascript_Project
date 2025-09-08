dd33440c50626980abdceeab7a1e1e0412f210e8
Hazrat Umar <13234hazrat@gmail.com>
2025-09-08 15:36:48 +0500

    complete the countdown function

--- project_03/app.js
+++ project_03/app.js
@@ -1,3 +1,29 @@
 const inputField = document.getElementById("input-field");
 const startBtn = document.getElementById("btn");
 const countElement = document.getElementById("count");
+
+
+let count = 0;
+let countId = null
+
+startBtn.addEventListener("click", () => {
+    const inputValue = Number(inputField.value);
+    if(!inputField.value || isNaN(inputValue)) alert("please put a number!")
+    startCounting(inputValue)
+
+    inputField.value = ""
+})
+
+function startCounting(counted){
+    clearInterval(countId)
+    countElement.innerText = counted
+    countId = setInterval( () => {
+        counted--;
+        if(counted < 0){
+            return clearInterval(countId)
+            
+        }
+        countElement.innerText = counted;
+    }, 1200)
+    
+}
