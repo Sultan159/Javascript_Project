53076eb8cae4be7810a105e42ca3ee12d896a9e3
Hazrat Umar <13234hazrat@gmail.com>
2025-09-09 09:31:59 +0500

    grab the element through DOM

---
+++ project_05/app.js
@@ -0,0 +1,7 @@
+const colorInput = document.getElementById("colorInput");
+const hexCode = document.getElementById("hexCode");
+const copyBtn = document.getElementById("copyBtn");
+const complementContainer = document.getElementById("complementryContainer");
+const saveBtn = document.getElementById("saveColorBtn");
+const favContainer = document.getElementById("favoriteContainer");
+
