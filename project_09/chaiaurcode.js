27d8ddf1d19b7e00a6ca4d84efa389daa43314ae
Hazrat Umar <13234hazrat@gmail.com>
2025-09-16 19:17:01 +0500

    fix the bugs

--- project_09/chaiaurcode.js
+++ project_09/chaiaurcode.js
@@ -1,6 +1,21 @@
-
-
-
-
+const generateBtn = document.querySelector(".btn");
+const containerImage = document.querySelector(".container");
+const div = document.createElement("div");
+const img = document.createElement("img");
 
 const url = "https://api.thecatapi.com/v1/images/search";
+
+
+
+async function generateRandomImage(){
+    const response = await fetch(url)
+    const data = await response.json();
+    div.classList.add("cats")
+    img.src = data[0].url;
+    img.classList.add("random_cats");
+    div.appendChild(img)
+    containerImage.appendChild(div)
+}
+
+generateBtn.addEventListener("click", generateRandomImage);
+
